09b50979be5c83e105140f1fe7529a3ad26e78cd
Peter <98979666+kingAuthor1212@users.noreply.github.com>
2024-09-19 12:01:35 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,6 +10,20 @@ class MyTestCase(unittest.TestCase):
     def test_add_mixed_numbers(self):
         self.assertEqual(addNumbers(1, -2), -1)
         self.assertEqual(addNumbers(-1, 2), 1)
+  def test_positive_numbers(self):
+        self.assertEqual(subNumbers(5, 3), 2)
 
+    def test_negative_numbers(self):
+        self.assertEqual(subNumbers(-5, -3), -2)
+
+    def test_zero(self):
+        self.assertEqual(subNumbers(0, 0), 0)
+        self.assertEqual(subNumbers(5, 0), 5)
+        self.assertEqual(subNumbers(0, 5), -5)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(subNumbers(10, 15), -5)
+        self.assertEqual(subNumbers(-10, 15), -25)
+        self.assertEqual(subNumbers(10, -15), 25)
 if __name__ == '__main__':
     unittest.main()
