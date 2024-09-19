daa11f214a950535e9708fb0c167b5c5b74ac6f2
Peter <98979666+kingAuthor1212@users.noreply.github.com>
2024-09-19 09:41:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
+import unittest
+from cal import addNumbers
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(addNumbers(1, 2), 3)
 
+    def test_add_negative_numbers(self):
+        self.assertEqual(addNumbers(-1, -2), -3)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(addNumbers(1, -2), -1)
+        self.assertEqual(addNumbers(-1, 2), 1)
+
+if __name__ == '__main__':
+    unittest.main()
